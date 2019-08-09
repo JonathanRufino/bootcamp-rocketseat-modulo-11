cd77ec6d882fa05ef2bab2075aaa9fc0557aeb68
Jonathan Rufino <jonathanrufinopaiva@gmail.com>
2019-08-08 21:10:47 -0300

    test: create user

---
+++ __tests__/integration/user.test.js
@@ -0,0 +1,17 @@
+import request from 'supertest';
+
+import app from '../../src/app';
+
+describe('User', () => {
+  it('should be able to register', async () => {
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Jonathan Rufino',
+        email: 'jonathan@email.com',
+        password_hash: '123456',
+      });
+
+    expect(response.body).toHaveProperty('id');
+  });
+});
